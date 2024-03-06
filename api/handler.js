268302be90c4be8500e95922af33b82eed9aeeb3
Luiz Felipe Wendt Campos <luizfelipew@Luizs-MacBook-Pro.local>
2024-03-05 21:06:37 -0300

    migrando a lógica da função

--- api/handler.js
+++ api/handler.js
@@ -1,13 +1,49 @@
-module.exports.handler = async (event) => {
+const { json } = require("stream/consumers")
+
+const previousResults = new Map()
+
+function extractBody(event) {
+    if (!event?.body) {
+        return {
+            statusCode: 422,
+            body: JSON.stringify({error: 'Missing body'})
+        }
+    }
+    return JSON.parse(event.body)
+}
+
+module.exports.sendResponse = async (event) => {
+
+    const { name, answers } = extractBody(event)
+    const correctQuestions = [3, 1, 0, 2]
+
+    const correctAnswers = answers.reduce((acc, answer, index) => {
+        if (answer === correctQuestions[index]) {
+            acc++
+        }
+        return acc
+    }, 0)
+
+    const result = {
+        name,
+        correctAnswers,
+        totalAnswers: answers.length
+    }
+
+    const resultId = randomUUID()
+    previousResults.set(resultId, { response: req.body, result })
+    
     return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "Go Serverless v3.0! Your function executed successfully!",
-          input: event,
-        },
-        null,
-        2
-      ),
-    };
-  };
+        statusCode: 201,
+        body: JSON.stringify({
+            resultId,
+            __hypermedia: {
+                href: `/results.html`,
+                query: { id: resultId }
+            }
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+}
